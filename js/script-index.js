204c82a9920462b65d6b64c44727460defe80d45
Maria Paz <maria.pazrojasc@gmail.com>
2017-07-10 16:51:32 -0400

    Etapa5: se crea condición, se vincula el data y se remueve la clase

--- js/script-index.js
+++ js/script-index.js
@@ -1,6 +1,7 @@
 $(document).ready( function(){
 	$('#back').hide();
 	//La variable "recipesArray" esta declarada en el archivo "data/recipes.js"
+	renderActivities(activities);
 	renderHighlightedRecipes(recipesArray);
 });
 
@@ -29,21 +30,25 @@ function renderHighlightedRecipes(recipesArray) {
 */
 function renderRecipe(recipe) {
 	console.log('Voy a pintar la receta: ', recipe);
-	$('.list-recipes').append('<a class="item-recipe" href="#"><span class="attribution"><span class="title-recipe">'+ recipe.title + '</span><span class="metadata-recipe"><span class="author-recipe">'+ recipe.source.title + '</span><span class="bookmarks-recipe"><span class="icon-bookmark"></span></span></span></span><img src="img/recipes/320x350/'+ recipe.name + '.jpg"></a>');
+	$('.list-recipes').append('<a class="item-recipe" href="#"><span class="attribution"><span class="title-recipe">'+ recipe.title + '</span><span class="metadata-recipe"><span class="author-recipe">'+ recipe.source.name + '</span><span class="bookmarks-recipe"><span class="icon-bookmark"></span></span></span></span><img src="img/recipes/320x350/'+ recipe.name + '.jpg"></a>');
 }
-
-
-
 /*
 * Función que se encarga de pintar todas las actividades
 */
-function renderActivities(activitiesArray) {
-	console.log('Activities: ', activitiesArray);
+function renderActivities(activities) {
+	console.log('Activities: ', activities);
+	activities.forEach(function(el){
+		console.log(el);
+		renderActivity(el);
+	});
+	if (activities.length > 0) {
+		$('#wrap').remove();
+	}
 }
 
 /*
 * Función que se encarga de pintar una actividad
-* Aqui se tiene que crear el HTML que esta en el 
+* Aqui se tiene que crear el HTML que esta en el
 * archivo "templates/templates-activity.html"
 */
 function renderActivity(recipe) {
